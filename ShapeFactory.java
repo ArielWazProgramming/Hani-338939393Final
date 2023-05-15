f910606725d35e7f94b6507e298dad9fb68751d2
Ariel Wazana <117109691+ArielWazProgramming@users.noreply.github.com>
2023-05-15 12:04:32 -0400

    Add files via upload

---
+++ ShapeFactory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Shapes;
+
+enum SHAPES {
+    CIRCLE, SQUARE, RECTANGLE
+}
+
+public class ShapeFactory {
+	
+   //use getShape method to get object of type shape 
+   public static Shape makeShape(String shapeType){
+      if(shapeType == null){
+         return null;
+      }		
+      if(shapeType.equalsIgnoreCase("CIRCLE")){
+         return new Circle();
+         
+      } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();
+         
+      } else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }
+      
+      return null;
+   }
+}
