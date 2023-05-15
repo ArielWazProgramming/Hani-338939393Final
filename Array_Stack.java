cb94e1336744290c4f6724e7cc09d799f6d1d38e
Ariel Wazana <117109691+ArielWazProgramming@users.noreply.github.com>
2023-05-15 16:18:16 -0400

    Update Array_Stack.java

--- Array_Stack.java
+++ Array_Stack.java
@@ -43,5 +43,39 @@ public class Array_Stack {
         
       s.display(); 
     }  
-  }
+
   
+  import java.util.Stack;
+
+public class BalancedBrackets {
+
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((top == '(' && ch != ')') || (top == '[' && ch != ']') || (top == '{' && ch != '}')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String expression = "{()}[]";
+        if (isBalanced(expression)) {
+            System.out.println(expression + " is balanced");
+        } else {
+            System.out.println(expression + " is not balanced");
+        }
+    }
+}
+
